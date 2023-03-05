fb549d3e3ae6b31559d2a5c7080a3e605b59e4e1
starodoubov <125405304+starodoubov@users.noreply.github.com>
2023-03-05 15:37:52 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,21 @@
+import random
+
+x = 0
+y = random.randint(1, 101)
+
+while True:    
+    x += 1
+    t = int(input())
+
+    if x == 20:
+        print("game over")
+        print("My number is %s" %y)
+        break
+    
+    if t == y:
+        print("you guess my number in %s tryes" %x)
+        break
+    elif t <= y:
+        print("Your number smaller than mine")
+    elif t >= y:
+        print("Your number bigger than mine try again")
